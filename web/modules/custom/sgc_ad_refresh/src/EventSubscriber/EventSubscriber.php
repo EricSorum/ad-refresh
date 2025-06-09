1e94999c4c1be8c80c86577fc22a0bfe49544dba
Eric Sorum <ejsorum@gmail.com>
2025-06-09 18:17:47 -0500

    Removed ResponseEvent import.

--- web/modules/custom/sgc_ad_refresh/src/EventSubscriber/EventSubscriber.php
+++ web/modules/custom/sgc_ad_refresh/src/EventSubscriber/EventSubscriber.php
@@ -4,7 +4,7 @@ namespace Drupal\sgc_ad_refresh\EventSubscriber;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\KernelEvents;
-use \Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use Drupal\Core\Render\AttachmentsInterface;
 
 class EventSubscriber implements EventSubscriberInterface {
